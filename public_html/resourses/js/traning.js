7a47453527a908b041b4e249f6972ed7aa5bd61e
Sergey <Sergey@192.168.0.12>
2016-06-27 16:05:54 -0700

    work

---
+++ public_html/resourses/js/traning.js
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function initialStartIn() {
+    document.getElementById("initialStart").style.transitionDelay = "2s";
+        document.getElementById('initialStart').style.marginBottom = '130px';
+        document.getElementById('firstPhoneCall').style.marginBottom = '130px';
+}
+function processVideosIn() {
+        document.getElementById('processVideos').style.marginBottom = '130px';
+        document.getElementById('pictureGuidelines').style.marginBottom = '130px';
+}
+function setUpSecondAppointmentIn() {
+        document.getElementById('setUpSecondAppointment').style.marginBottom = '130px';
+        document.getElementById('attendSecondAppointment').style.marginBottom = '130px';
+}
+
+/*function initialStartOut() {
+        document.getElementById('initialStart').style.marginBottom = '10px';
+        document.getElementById('firstPhoneCall').style.marginBottom = '10px';
+        document.getElementById('processVideos').style.marginBottom = '10px';
+        document.getElementById('pictureGuidelines').style.marginBottom = '10px';
+        document.getElementById('setUpSecondAppointment').style.marginBottom = '10px';
+        document.getElementById('attendSecondAppointment').style.marginBottom = '10px';
+}*/
